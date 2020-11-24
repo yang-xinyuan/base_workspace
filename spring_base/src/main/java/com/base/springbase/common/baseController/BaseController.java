ccbee95de3c7eee0bdf2b2954cc3cec3d3424622
xinyuan <1046916988@qq.com>
2020-11-24 18:10:56 +0800

    修改项目的auto项目的groupId 为com.base

--- spring_base/src/main/java/com/base/springbase/common/baseController/BaseController.java
+++ spring_base/src/main/java/com/base/springbase/common/baseController/BaseController.java
@@ -1,7 +1,6 @@
 package com.base.springbase.common.baseController;
 
-import com.auto.autoconfig.common.config.domain.DemoDomain;
-import com.base.springbase.common.autoconfig.domain.School;
+import com.base.autoconfig.common.config.domain.DemoDomain;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
